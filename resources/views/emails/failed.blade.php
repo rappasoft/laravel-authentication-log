014cfef3ffa06560e96ba539e6580b14e8cccd4d
robertosanchez <roberto.sanchez@4foreverything.com>
2025-05-23 09:53:48 +0200

    Fixed default value when state location is not available

--- resources/views/emails/failed.blade.php
+++ resources/views/emails/failed.blade.php
@@ -8,7 +8,7 @@
 > **@lang('IP Address:')** {{ $ipAddress }}<br/>
 > **@lang('Browser:')** {{ $browser }}<br/>
 @if ($location && $location['default'] === false)
-> **@lang('Location:')** {{ $location['city'] ?? __('Unknown City') }}, {{ $location['state'], __('Unknown State') }}
+> **@lang('Location:')** {{ $location['city'] ?? __('Unknown City') }}, {{ $location['state'] ?? __('Unknown State') }}
 @endif
 
 @lang('If this was you, you can ignore this alert. If you suspect any suspicious activity on your account, please change your password.')
